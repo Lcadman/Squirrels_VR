5bdfdaf8a9cd5bb385936944cec3acf0fbadcad8
MunchGuy <makio.man.co@gmail.com>
2022-11-06 12:30:17 -0700

    Add Mountains

---
+++ SquirrelXRtoolkit/Assets/SkyController.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SkyController : MonoBehaviour
+{
+
+    private Material Skybox_9;
+
+    private float Tfactor;
+    public bool isNight;
+
+    public float NightValue;
+    public float DayValue;
+
+    public float RotationSpeed;
+    public float ChangeSpeed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        Skybox_9 = RenderSettings.skybox;
+
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        Skybox_9.SetFloat("_Rotation", Skybox_9.GetFloat("_Rotation") + Time.deltaTime * RotationSpeed);
+
+        if(isNight == true && Tfactor >= NightValue)
+        {
+
+            Tfactor -= Time.deltaTime * ChangeSpeed;
+
+            Skybox_9.SetColor("_Tint", new Color(Tfactor, Tfactor, Tfactor, 1));
+        }
+        else if (isNight == false && Tfactor <= DayValue)
+        {
+            Tfactor += Time.deltaTime * ChangeSpeed;
+
+            Skybox_9.SetColor("_Tint", new Color(Tfactor, Tfactor, Tfactor, 1));
+
+        }
+
+
+    }
+}
