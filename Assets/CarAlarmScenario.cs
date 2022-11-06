dd7eab3fde550f9819d94fb392e222a743046a06
benschra <schraeder.br@gmail.com>
2022-11-06 01:21:47 -0700

    Car alarm updated

---
+++ Assets/CarAlarmScenario.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+
+public class CarAlarmScenario : MonoBehaviour {
+	public List<UnityEvent> events;
+	public float delay = 5.0f;
+	// Start is called before the first frame update
+	void Start() {
+		//StartCoroutine(runScenario());
+	}
+
+	public IEnumerator runScenario() {
+		print("ran");
+		events[0].Invoke();
+		yield return new WaitForSeconds(delay);
+		events[1].Invoke();
+		yield return new WaitForSeconds(delay);
+		events[2].Invoke();
+		yield return new WaitForSeconds(delay);
+
+
+	}
+
+	public void StartScenario() {
+		StartCoroutine(runScenario());
+	}
+	// Update is called once per frame
+	void Update() {
+
+	}
+}
